59683d184f6528f706df68c90c19a7e0a527a6be
zulfikar6556 <zulfikar6556@gmail.com>
2025-02-07 14:09:18 +0000

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer';
@@ -46,6 +47,7 @@ export default function RootLayout({
           <main>
             {children}
             <Analytics />
+            <SpeedInsights/>
             <Toaster />
           </main>
           <Footer />
