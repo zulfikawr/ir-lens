6dd98ea3e678b663e48d27c4bd78454ffd808a21
zulfikar6556 <zulfikar6556@gmail.com>
2025-02-08 11:05:26 +0000

    fix slug api

--- app/api/article/[slug]/route.ts
+++ app/api/article/[slug]/route.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { getArticleBySlug } from '@/lib/database';
 import type { ArticleType } from '@/types/article';
 
-export async function GET(
-  request: Request,
-  { params }: { params: { slug: string } },
-) {
+type Props = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export async function GET(request: NextRequest, props: Props) {
   try {
+    const params = await props.params;
     const { slug } = params;
 
     if (!slug) {
@@ -17,14 +21,13 @@ export async function GET(
     }
 
     const article = await getArticleBySlug(slug);
-
     if (!article) {
       return NextResponse.json({ error: 'Article not found' }, { status: 404 });
     }
 
     return NextResponse.json({
       articles: [article],
-    } satisfies ArticleType);
+    } as ArticleType);
   } catch (error) {
     console.error('Error fetching article:', error);
     return NextResponse.json(
